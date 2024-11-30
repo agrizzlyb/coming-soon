7bbb66d6745a84715e5f485af0594e6aefae9b74
AlexGBear <alexgbear@outlook.com>
2024-11-30 16:36:49 +0800

    changed favicon take 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 // src/pages/_app.tsx
 import "../styles/global.css";  // Ensure this path is correct
 import { AppProps } from "next/app";
+import Head from "next/head";  // Import Head from Next.js
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Favicon link */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
